101c741646ca609e3eacafc49d266798c66d2b5e
italoperez2019 <iep4@njit.edu>
2018-11-01 12:32:23 +0500

    Create a unit test to test to count the number of records inserted by the database seed.  i.e. $carCount = 50

---
+++ tests/Unit/CountCarTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Car;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CountCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCountCarsIs50()
+    {
+        $carCount = 50;
+        $cars = factory(Car::class, 50 )->create();
+
+        // Assert that the number of queries are below a certain point.
+        $this->assertLessThanOrEqual( $carCount, \count( $cars ) );
+        $this->assertTrue(true);
+    }
+}
